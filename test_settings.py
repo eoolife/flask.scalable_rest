b85b6f2db4a7d9f9e9be023d0a2fa8171e14d964
嘿哟传说 <eoolife@163.com>
2015-09-17 21:25:17 +0800

    1. change uwsgi name to Bootstrap

--- test_settings.py
+++ test_settings.py
@@ -12,9 +12,8 @@
 from datetime import timedelta
 
 DEBUG = True
-USE_DIESEL = False
 HOST = '0.0.0.0'
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123456@localhost:3306/restapi?charset=utf8mb4'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123456@192.168.1.103:3316/restapi?charset=utf8mb4'
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_POOL_RECYCLE = 20
 SECRET_KEY = 'super-secret fuck'
